12709669da2ff7a2ccb4c4ff629a79a7a53c55c4
meganpaffrath <meganpaffrath@gmail.com>
2021-02-14 20:35:27 -0800

    footer format

--- frontend/src/components/layout/Footer.jsx
+++ frontend/src/components/layout/Footer.jsx
@@ -1,4 +1,9 @@
-import React from 'react'
+import React from 'react';
+// BOOTSTRAP
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import { Row, Button, Form, ButtonGroup } from 'react-bootstrap';
+import { Github, Linkedin } from 'react-bootstrap-icons';
+import './footer.css';
 
 export default function Footer() {
   let year = new Date().getFullYear();
@@ -6,7 +11,11 @@ export default function Footer() {
   return (
     <footer className="footer">
       <h5>&copy; Megan Paffrath {year}</h5>
-      <p>meganpaffrath.com</p>
+      <p>
+        <a href="https://www.meganpaffrath.com/" target="_blank">meganpaffrath.com</a>
+        <a href="https://www.meganpaffrath.com/" target="_blank"><Github /></a>
+        <a href="https://www.meganpaffrath.com/" target="_blank"><Linkedin /></a>
+      </p>
     </footer>
   )
 }
