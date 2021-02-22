7842b8583be1fdd1b38347ee06aa7a1be1a74a8d
meganpaffrath <meganpaffrath@gmail.com>
2021-02-22 15:26:09 -0800

    page redirects properly

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -19,55 +19,6 @@ export default function App() {
     username: undefined
   });
 
-  useEffect(() => {
-    const verifyUser = async () => {
-      let token = localStorage.getItem("auth-token");
-      if (token === null) {
-        localStorage.setItem("auth-token", "");
-        token = "";
-      }
-
-      if (token) {
-        try {
-          // check with db if token is valid
-          let ts = new Date(Date.now());
-          const tokenRes = await Axios.post(
-            (process.env.REACT_APP_API_URL + "/users/validateToken"),
-            {},
-            {headers: {
-              "x-auth-token": token,
-              'Content-Type': 'application/json',
-              'Cache-Control' : 'no-cache',
-              time: ts
-            }}
-          );
-
-          if (tokenRes) {
-            const userRes = await Axios.get(
-              (process.env.REACT_APP_API_URL + "/users/"),
-              {headers: {
-                "x-auth-token": token,
-                'Content-Type': 'application/json',
-                'Cache-Control' : 'no-cache',
-                time: ts
-              }}
-            );
-            setUserData({
-              id: userRes.data.id,
-              username: userRes.data.username
-            });
-          }   
-        } catch (err) {
-          localStorage.setItem("auth-token", "");
-          token = "";
-        }
-      }
-    }
-    
-    verifyUser();
-    
-  }, []);
-
   return (
     <>
       <BrowserRouter>
--- frontend/src/components/auth/AuthMenu.jsx
+++ frontend/src/components/auth/AuthMenu.jsx
@@ -2,17 +2,65 @@ import React, { useEffect, useContext } from 'react';
 import { useHistory } from 'react-router-dom';
 import UserContext from '../../context/UserContext';
 import {Nav, NavDropdown} from 'react-bootstrap';
+import axios from 'axios';
 
 // determins what user sees if logged in/out
 export default function AuthMenu() {
   const {userData, setUserData} = useContext(UserContext);
   const history = useHistory();
 
-  // useEffect(() => {
-  //   if (!userData.username) {
-  //     history.push("/login");
-  //   }
-  // });
+  useEffect(() => {
+    const verifyUser = async () => {
+      let token = localStorage.getItem("auth-token");
+      if (token === null) {
+        localStorage.setItem("auth-token", "");
+        token = "";
+        // redirect user
+        history.push("/login");
+      }
+
+      if (token) {
+        try {
+          // check with db if token is valid
+          let ts = new Date(Date.now());
+          const tokenRes = await axios.post(
+            (process.env.REACT_APP_API_URL + "/users/validateToken"),
+            {},
+            {headers: {
+              "x-auth-token": token,
+              'Content-Type': 'application/json',
+              'Cache-Control' : 'no-cache',
+              time: ts
+            }}
+          );
+
+          if (tokenRes) {
+            const userRes = await axios.get(
+              (process.env.REACT_APP_API_URL + "/users/"),
+              {headers: {
+                "x-auth-token": token,
+                'Content-Type': 'application/json',
+                'Cache-Control' : 'no-cache',
+                time: ts
+              }}
+            );
+            setUserData({
+              id: userRes.data.id,
+              username: userRes.data.username
+            });
+          }   
+        } catch (err) {
+          localStorage.setItem("auth-token", "");
+          token = "";
+          // redirect user
+          history.push("/login");
+        }
+      }
+    }
+    
+    verifyUser();
+    
+  }, []);
 
   const register = () => { history.push("/register")};
   const login = () => { history.push("/login")};
